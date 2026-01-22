aedd8b9722f0169d884250cca2c5652cfe513be7
Ilya <assa.tresh08@gmail.com>
2026-01-22 15:12:13 +0500

    Add comments examples

---
+++ python-basics/comments.py
@@ -0,0 +1,5 @@
+# Comments starts with a #, and Python will ignore them:
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+
+print("Hello, World!") #This is a comment
