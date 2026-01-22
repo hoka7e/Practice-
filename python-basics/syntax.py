79c1b7de5265b3f1e8011bfdc1953626819ab39e
Ilya <assa.tresh08@gmail.com>
2026-01-22 15:15:24 +0500

    Add syntax examples

---
+++ python-basics/syntax.py
@@ -0,0 +1,11 @@
+if 5 > 2:
+    print("Five is greater than two!")
+
+if 5 > 2:
+        print("Five is greater than two!")
+#Statements 
+print("Hello World!")
+
+print("Have a good day.")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
