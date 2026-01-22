fad1415ce4dc056d68a10a77aa60cb9609222e9b
Ilya <assa.tresh08@gmail.com>
2026-01-22 15:14:27 +0500

    Add output examples

---
+++ python-basics/output.py
@@ -0,0 +1,13 @@
+print("This will work!")
+
+print('This will also work!')
+
+print("Hello World!", end=" ") #print without a new line
+print("I will print on the same line.")
+
+#print numbers
+print(3)
+
+print(358)
+
+print(3 + 3)
