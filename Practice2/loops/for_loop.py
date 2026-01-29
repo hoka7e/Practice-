85539daee6c0cc866a2aeff3f6935a4133be38b9
Ilya <assa.tresh08@gmail.com>
2026-01-29 12:13:23 +0500

    Types of loops and examples

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,21 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "banana":
+  print(x)
+
+
+for x in range(6):
+  print(x)
+
+for x in range(2, 6):
+  print(x)
+
+for x in range(2, 30, 3): #Increment the sequence with 3 (default is 1):
+  print(x)
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
